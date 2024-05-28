53d12d94d1469be3d1edcb2218e182e93a5378bc
Simon <vonahkahs.s@gmail.com>
2024-05-28 17:35:36 +0400

    add decrypt file

--- main.py
+++ main.py
@@ -27,8 +27,35 @@ def encrypt_file():
 	print(f'[+] Created - {encrypted_file_name}')
 
 
+# File decryption
+def decrypt_file():
+	file_path = input('[+] Enter file path: ')
+	password = input('[+] Enter password: ')
+	decrypted_file_name = input('[+] Enter a name for the decrypted file: ')
+
+	try:
+		file_reader = PdfReader(file_path)
+	except FileNotFoundError:
+		print(f'[INFO] No file with path: {file_path}')
+		sys.exit()
+
+	if file_reader.is_encrypted:
+		file_reader.decrypt(password)
+
+	file_writer = PdfWriter()
+
+	for page in range(len(file_reader.pages)):
+		file_writer.add_page(file_reader.pages[page])
+
+	with open(decrypted_file_name, 'wb') as file:
+		file_writer.write(file)
+
+	print(f'[+] Created - {decrypted_file_name}')
+
+
 def main():
-	encrypt_file()
+	choose = input('[+] Enter 0 to encrypt the file, enter 1 to decrypt: ')
+	encrypt_file() if choose == '0' else decrypt_file()
 
 
 if __name__ == '__main__':
